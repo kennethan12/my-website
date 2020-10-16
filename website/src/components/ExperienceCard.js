8d24b50705c08a0aaa7469e0be96ab2262de0a6e
Kenneth An <ta7@williams.edu>
2020-10-16 16:01:24 -0400

    finished ExperiencePage

--- website/src/components/ExperienceCard.js
+++ website/src/components/ExperienceCard.js
@@ -6,27 +6,57 @@ class ExperienceCard extends Component {
     let { logo, company, position, dates, description } = this.props;
 
     return (
-      <div class="container-fluid">
-        <div className="experience">
-          <div class="row">
-            <div id="logoCol" class="col-lg-1">
-              <img class="center" src={logo}></img>
+      // <div class="container-fluid">
+      //   <div className="experience">
+      // <div class="row">
+      //   <div id="logoCol" class="col-1">
+      //     <img class="center" src={logo}></img>
+      //   </div>
+      //   <div class="col-11">
+      //     <div class="row">
+      //       <p>{company}</p>
+      //     </div>
+      //     <div class="row">
+      //       <div class="col">
+      //         <p>{position}</p>
+      //       </div>
+      //       <div class="col">
+      //         <p>{dates}</p>
+      //       </div>
+      //     </div>
+      //     <div class="row">
+      //       {description}
+      //     </div>
+      //   </div>
+      // </div>
+      //   </div>
+      // </div>
+      <div class="container-experience">
+        <div class="row">
+          <div id="logoCol" class="img-experience">
+            <img class="center" src={logo}></img>
+          </div>
+          <div class="company-experience">
+            <div class="row pt-1 pl-3 pr-3">
+              <p class="mb-0" style={{
+                fontWeight: "500",
+                fontSize: "1.3em",
+                lineHeight: '100%'
+              }}>
+                {company}
+              </p>
             </div>
-            <div class="col-lg-11">
-              <div class="row">
-                <p>{company}</p>
+            <div class="row pl-3 pr-3">
+              <div class="col-sm pl-0">
+                <p id="experience-position">{position}</p>
               </div>
-              <div class="row">
-                <div class="col">
-                  <p>{position}</p>
-                </div>
-                <div class="col">
-                  <p>{dates}</p>
-                </div>
+              <div id="experience-dates"
+                class="col-sm pl-0 pr-5">
+                <p style={{ color: '#4B6B82' }}>{dates}</p>
               </div>
-              <div class="row">
-                {description}
-              </div>
+            </div>
+            <div class="row pl-3 pr-5">
+              {description}
             </div>
           </div>
         </div>
--- website/src/components/ExperiencePage.js
+++ website/src/components/ExperiencePage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import ExperienceCard from './ExperienceCard'
-import mthreeLogo from '../images/mthree.png'
+import ExperienceCard from './ExperienceCard';
+import mthreeLogo from '../images/mthree.png';
+import ftlLogo from '../images/ftl.png';
+import appstraxLogo from '../images/appstrax.png';
+import dots from '../images/dots.png'
 
 class ExperiencePage extends Component {
   state = {
@@ -32,11 +35,66 @@ class ExperiencePage extends Component {
           back-end, and front-end together to create full stack applications.
         </p>
       </div>
+    },
+    ftl: {
+      logo: ftlLogo,
+      company: "Faster Than Light",
+      position: "Software/Business Development Intern",
+      dates: "June 2019 - August 2019",
+      description: <div>
+        <p>
+          Faster Than Light is a cybersecurity startup dedicated to providing a fast
+          and easy-to-use online static analysis tool for programmers of any level.
+        </p>
+        <p>
+          Faster Than Light exposed me to what it truly meant to work in a start-up
+          environment. I wore many hats throughout my internship. I performed lots of
+          user testing, many of which provided many feedback about the UI/UX of the
+          startup’s BugCatcher product. I was assigned to think of useful features that
+          could be tailored more towards amateurs and student coders, such as the
+          integration of Github. Everyone in the team did quality assurance tests with
+          every product iteration. I had to relearn Python to help debug the source
+          code of the product. And lastly, I dedicated half of my time to reaching out
+          to online developer communities such as blogs, podcasts, and Meetups for
+          outreach purposes.
+        </p>
+        <p>
+          The three most valuable skills I’ve gained throughout my experience was
+          communication, organization, and teamwork. Because the internship was remote,
+          I made sure to not bombard my boss with questions, since she was always busy
+          with meetings. I only reached out whenever I couldn’t figure out the answers
+          myself. I also made sure to keep a tight schedule, since I constantly had
+          multiple tasks to keep track of. I learned how to be brief and succinct in
+          cold emails. And lastly, I always updated my team with my progress.
+        </p>
+      </div>
+    },
+    appstrax: {
+      logo: appstraxLogo,
+      company: "AppStrax Technology (Pyt) Ltd",
+      position: "Full Stack Developer Intern",
+      dates: "July 2018 - August 2018",
+      description: <div>
+        <p>
+          AppStrax Technology is a full stack software development team dedicated to
+          providing technical solutions to its clients. I was fortunate to work as a full
+          stack intern under Miki, Co-founder and a full stack instructor of a bootcamp I
+          was attending called iXperience.
+        </p>
+        <p>
+          The AppStrax internship was my first tech industry experience as a coder. The
+          fellow interns and I first worked on a general accounting API that would alleviate
+          transaction traffic that one of AppStrax’s clients was facing. I worked heavily on
+          the backend using MySQL and Loopback, a Node.js and TypeScript framework. I also
+          worked on making basic CRUD functionalities for another one of AppStrax’s clients,
+          Exmural, using the same language and framework.
+        </p>
+      </div>
     }
   }
 
   render() {
-    let { mthree } = this.state
+    let { mthree, ftl, appstrax } = this.state
 
     return (
       <div class="container-fluid" style={{ backgroundColor: "#E2D5C0", flexWrap: "wrap" }}>
@@ -49,13 +107,37 @@ class ExperiencePage extends Component {
           <div class="row">
             <div className="underscore-dark" />
           </div>
-          <div class="row">
+          <div class="row mt-4">
             <ExperienceCard
               logo={mthree.logo}
               company={mthree.company}
               position={mthree.position}
               dates={mthree.dates}
               description={mthree.description}
+            />
+          </div>
+          <div class="row">
+            <img id="dots" class="center" src={dots}></img>
+          </div>
+          <div class="row">
+            <ExperienceCard
+              logo={ftl.logo}
+              company={ftl.company}
+              position={ftl.position}
+              dates={ftl.dates}
+              description={ftl.description}
+            />
+          </div>
+          <div class="row">
+            <img id="dots" class="center" src={dots}></img>
+          </div>
+          <div class="row">
+            <ExperienceCard
+              logo={appstrax.logo}
+              company={appstrax.company}
+              position={appstrax.position}
+              dates={appstrax.dates}
+              description={appstrax.description}
             />
           </div>
         </div>
